e13ddeb26950b6a189c76f64bd532375af3a29f3
goeuri-rey <115942541+goeuri-rey@users.noreply.github.com>
2022-11-21 17:50:48 +0900

    ボタン機能追加 クリックイベント追加

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 import "./styles.css";
+
+const onClickAdd = () => {
+  alert();
+};
+
+document
+  .getElementById("add-button")
+  //対象の要素.addEventListener(種類, 関数);
+  .addEventListener("click", () => onClickAdd());
